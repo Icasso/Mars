2204eedb18daf5b954e0f331cfe46ace5d06b05c
Isaac Tsui <isaactsui2000@gmail.com>
2022-12-03 21:25:50 +0800

    [2022-12-03]: Add Binance - US config

--- client/BinanceClient.py
+++ client/BinanceClient.py
@@ -1,12 +1,15 @@
 import requests
 import streamlit as st
 
-BASE_URL = "https://api.binance.com"
+BASE_URL_INTERNATIONAL = "https://api.binance.com"
+BASE_URL_US = "https://api.binance.us"
 
 
 @st.cache(ttl=5, suppress_st_warning=True)
-def get_status():
-    request = BASE_URL + '/sapi/v1/system/status'
+def get_status(region):
+    request = BASE_URL_INTERNATIONAL + '/sapi/v1/system/status'
+    if region == "US":
+        request = BASE_URL_US + '/sapi/v1/system/status'
     st.code(request)
     response = requests.get(request).json()
     st.json(response)
